0c0003fe9ebbe0a6db36dc20df0d35a431a373dd
d-dimitrovw <daviddimitrov2004@gmail.com>
2024-12-19 13:38:24 +0200

    added home, login and register pages

--- exam-project/src/app/app.routes.ts
+++ exam-project/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './user/register/register.component';
+import { LoginComponent } from './user/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent },
+
+
+];
