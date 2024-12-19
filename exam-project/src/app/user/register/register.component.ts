bd5131f13c96bfca1f3aa6321dc0a8cdcc8d1d0b
d-dimitrovw <daviddimitrov2004@gmail.com>
2024-12-20 01:05:47 +0200

    form validation

--- exam-project/src/app/user/register/register.component.ts
+++ exam-project/src/app/user/register/register.component.ts
@@ -1,28 +1,46 @@
 import { Component } from '@angular/core';
-import { FormsModule, NgForm } from '@angular/forms';
+import { FormBuilder, FormControl, ReactiveFormsModule, FormGroup, FormsModule, Validators } from '@angular/forms';
 import { Router, RouterLink } from '@angular/router';
 import { UserService } from '../user-service.service';
+import { matchPasswordsValidator } from '../match-passwords.validator';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [RouterLink, FormsModule],
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
-  constructor(private userService: UserService, private router: Router){}
-  register(form: NgForm) {
-    if (form.invalid) {
+  form = new FormGroup({
+    username: new FormControl('', [Validators.required, Validators.minLength(5)]),
+    email: new FormControl('', [Validators.required]),
+    tel: new FormControl(''),
+    passGroup: new FormGroup(
+      {
+        password: new FormControl('', [Validators.required, Validators.minLength(5)]),
+        rePassword: new FormControl('', [Validators.required]),
+      },
+      {
+        validators: [matchPasswordsValidator('password', 'rePassword')]
+      }
+    ),
+
+  })
+  constructor(private userService: UserService, private router: Router, private fb: FormBuilder){}
+  
+
+  register() {
+    if (this.form.invalid) {
       console.log('Invalid form');
       
       return;
     }
 
-    const {username, email, tel, password, rePassword} = form.value;
+    const {username, email, tel, passGroup: {password, rePassword} = {}} = this.form.value;
 
     this.userService
-    .register(username, email, tel, password, rePassword)
+    .register(username!, email!, tel!, password!, rePassword!)
     .subscribe(() => {
       this.router.navigate(['/home'])
     });
